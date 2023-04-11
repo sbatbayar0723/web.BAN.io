6eb7883d55ecbaf746c6f5eace04ab598da226ed
sbatbayar0723 <124324103+sbatbayar0723@users.noreply.github.com>
2023-04-11 18:53:29 +0800

    changes

---
+++ Index.js
@@ -0,0 +1,11 @@
+
+const aboutUsLink = document.querySelector('a[href="#aboutus"]');
+
+const aboutUsSection = document.querySelector('.aboutUs');
+
+aboutUsLink.addEventListener('click', function(event) {
+  
+  event.preventDefault();
+
+  aboutUsSection.scrollIntoView({ behavior: 'smooth' });
+});

1e798c71c07cc884ebfbc25b7415fada90c9e185
sbatbayar0723 <124324103+sbatbayar0723@users.noreply.github.com>
2023-05-06 15:06:08 +0800

    contact final

--- Javascript/Index.js
+++ Javascript/Index.js
@@ -20,6 +20,27 @@ fetch('index.json')
    aboutUsSection.scrollIntoView({ behavior: 'smooth' });
  });
 
+// Get the Contact Us link in the navigation menu
+var contactLink = document.getElementById("contact-link");
+
+// Add an event listener to the link
+contactLink.addEventListener("click", function(event) {
+  // Prevent the default behavior of the link
+  event.preventDefault();
+
+  // Get the position of the Contact Information section on the page
+  var contactSection = document.getElementById("contact");
+  var contactPosition = contactSection.offsetTop;
+
+  // Scroll to the Contact Information section
+  window.scrollTo({
+    top: contactPosition,
+    behavior: "smooth"
+  });
+});
+
+
+
  function toggleNav() {
   var x = document.getElementById("mySidenav");
   if (x.style.height === "auto") {
